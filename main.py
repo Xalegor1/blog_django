7175dafa7fc8c0c243ac9f644146d3c72cb6141e
Xalegor <Argus1672@gmail.com>
2022-07-03 16:27:33 +0500

    Finish

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-print('hello world')
+import string
+
+ltrs = string.ascii_lowercase
+nums = [i for i in range(10, 46)]
+res = zip(nums, ltrs)
+# print(list(ltrs))
+for i in res:
+    print(i)
+# print(list(res))
 
 
